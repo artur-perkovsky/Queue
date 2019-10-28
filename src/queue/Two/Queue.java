18ea2add22664d2cd7c39a663b5f2cb35f9bbf6c
artur-perkovsky <perkovsky.artur@gmail.com>
2019-10-28 23:31:50 +0300

    Fix

--- src/queue/Two/Queue.java
+++ src/queue/Two/Queue.java
@@ -1,62 +1,68 @@
 package queue.Two;
 
+
 public class Queue {
 
     private int putLoc; //Индекс записи элементов в очередь
     private int getLoc; //Индекс чтение элементов из очереди
     private char[] array; // Массив для очереди
-    private char a;
+    private int putLocEnd; //Идикатор конца строки (записи)
 
     //конструктор
     Queue(int sizeArray) {
         putLoc = 0;
         getLoc = 0;
         array = new char[sizeArray];
-        
+        putLocEnd = 0;
     }
 
     //Метод записи в очередь
-    public void get(char value) {
-        resetLoc();
-        if (getLoc <= array.length - 1) {
-            array[getLoc] = value;
-            getLoc++;
-        } else if (check()) {
-            getLoc = 0;
-            if (getLoc > putLoc) {
-                array[getLoc] = value;
-                getLoc++;
+    public void put(char value) {
+        if (putLocEnd != 0) {
+            if (putLoc < getLoc) {
+                zeroingLoc();
+                record(value);
+            } else {
+                System.out.println("This Queue is Full");
             }
+        } else if (putLoc <= array.length - 1) {
+            zeroingLoc();
+            record(value);
         } else {
-            System.out.println("The Queue is Full");
-            return;
+            if (getLoc != 0) {
+                putLocEnd = 1;
+                putLoc = 0;
+                zeroingLoc();
+                record(value);
+            } else {
+                System.out.println("This Queue is Full");
+            }
         }
     }
-
-    //Проверка индекса чтение ядля обнуления индекса записи
-    private boolean check() {
-        return putLoc != 0;
+    //Запись элемента в массив
+    private void record(char value) {
+        array[putLoc] = value;
+        putLoc++;
     }
 
-    //Обнуления ондесов
-    private void resetLoc() {
+    //Обнуления индексов
+    private void zeroingLoc() {
         if (getLoc == putLoc) {
             getLoc = 0;
             putLoc = 0;
+            putLocEnd = 0;
         }
     }
 
     //Чтение из очереди
-    public char put() {
-        if ((putLoc <= array.length - 1) & (putLoc < getLoc)) {
-            a = array[putLoc];
-            array[putLoc] = ' ';
-            putLoc++;
-            return a;
+    public char get() {
+        if ((getLoc <= array.length - 1) && (getLoc < putLoc)) {
+            return array[getLoc++];
         } else {
             System.out.println("The Queue is Empty");
             return ' ';
         }
-
     }
+
+
 }
--- src/queue/Two/QueueDemo.java
+++ src/queue/Two/QueueDemo.java
@@ -3,17 +3,17 @@ package queue.Two;
 public class QueueDemo {
     public static void main(String[] args) {
 
-        Queue q1 = new Queue(5);
+        Queue q1 = new Queue(7);
         char ch;
 
         //Заполнить очередь
-        for (int i = 0; i < 3; i++) {
-            q1.get((char) ('a' + i));
+        for (int i = 0; i < 4; i++) {
+            q1.put((char) ('a' + i));
         }
 
         //Чтение очереди
-        for (int i = 0; i < 5; i++) {
-            ch = q1.put();
+        for (int i = 0; i < 4; i++) {
+            ch = q1.get();
             if (ch != ' '){
                 System.out.print(ch + " ");
             }
@@ -21,8 +21,8 @@ public class QueueDemo {
         }
 
         //Заполнить очередь повторно
-        for (int i = 0; i < 5; i++) {
-            q1.get((char) ('z' - i));
+        for (int i = 0; i < 7; i++) {
+            q1.put((char) ('z' - i));
         }
 
         System.out.println();
